378e2d6f8064e58d9639e3498ad8e48128592084
correioch <correioch@gmail.com>
2019-03-29 17:22:16 -0400

    projeto criado

---
+++ src/main/java/com/ch/curso_spring/SpringChApplication.java
@@ -0,0 +1,13 @@
+package com.ch.curso_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChApplication.class, args);
+	}
+
+}
