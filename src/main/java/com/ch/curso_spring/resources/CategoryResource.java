7f77fba7a06a57fa62c04922195fbfc3634dcd03
correioch <correioch@gmail.com>
2019-03-29 19:22:24 -0400

    adicionado categoria

--- src/main/java/com/ch/curso_spring/resources/CategoryResource.java
+++ src/main/java/com/ch/curso_spring/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.ch.curso_spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ch.curso_spring.domain.Category;
 
 @RestController
 @RequestMapping(value="categories")
 public class CategoryResource {
 	
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "test ok";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "infomatique");
+		Category cat2 = new Category(2, "bureau");
+		
+		List<Category> listCategory = new ArrayList<>();
+		listCategory.add(cat1);
+		listCategory.add(cat2);
+		
+		return listCategory;
 	}
 
 }
